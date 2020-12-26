98403765644225494842139493e7caedf9fffd41
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-12-26 16:23:01 +0200

    +++

--- LB_1/LB_1.cpp
+++ LB_1/LB_1.cpp
@@ -1,1 +1,33 @@
-﻿fgfgf
+﻿#include <iostream>
+#include <conio.h>
+#include <time.h>
+
+using namespace std;
+
+#define N 8
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+
+	int arr[N]{};
+
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = 0;
+		cout << arr[i] << endl;
+	}
+	_getch();
+	system("cls");
+
+			for (int i = 0; i < N; i++)
+			{
+				arr[i] = rand() % 100;
+				  cout << arr[i] << endl;
+			}
+
+	
+
+
+}
